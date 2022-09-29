17c16edcdb2b9c392240fc4769e6141352413a98
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-09-29 08:20:43 -0700

    dynamic routes test 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import InvoiceView from "../views/InvoiceView.vue";
 import { auth } from "@/firebase";
 
 const router = createRouter({
@@ -19,7 +20,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/InvoiceView.vue"),
+      component: InvoiceView,
+      // component: () => import("../views/InvoiceView.vue"),
     },
     {
       path: "/login",
